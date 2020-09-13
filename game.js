aab2e8df205a13e688e91bc004cd38efb3e1326d
Gabriele <gabbopg13@gmail.com>
2020-09-13 16:47:10 +0200

    fixed

--- game.js
+++ game.js
@@ -1,27 +1,27 @@
-let canvas;
-let canvasW = 1400;
-let canvasH = 1000;
-let ctx;
 let player;
 let bullets = [];
-let asteroids = [];
+let obstacles = [];
 let keys = [];
 let score = 0;
 let lives = 3;
 
+window.addEventListener("click", game);
 function game(){
-    canvas = document.getElementById("myCanvas");
-    ctx = canvas.getContext("2d");
-    canvas.width = canvasW;
-    canvas.height = canvasH;
+    var canvas = document.getElementById("space");
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.canvasW = 1500;
+    this.canvasH = 900;
+    this.canvas.width = this.canvasW;
+    this.canvas.height = this.canvasH;
+    window.addEventListener("keydown", keyDown);
+    window.addEventListener("keyup", keyUp);
+
     player = new Player();
- 
-    for(let i = 0; i < 5; i++){
-        asteroids.push(new Asteroid());
+    for(let i = 0; i < 8; i++){
+        obstacles.push(new Obstacle());
     }
 
-    document.body.addEventListener("keydown", keyDown);
-    document.body.addEventListener("keyup", keyUp);
     render();
 }
 
@@ -30,7 +30,7 @@ function keyDown(i){
 }
 function keyUp(i){
     keys[i.keyCode] = false;
-    if (i.keyCode === 32){
+    if (i.keyCode == 32){
         bullets.push(new Bullet(player.angle));
     }
 }
@@ -44,22 +44,21 @@ function collision(ax, ay, r1, bx, by, r2){
     }
 }
  
-class Player{
-    constructor(){
-        this.visible = true;
-        this.x = canvasW / 2;
-        this.y = canvasH / 2;
-        this.vertX = this.x;
-        this.vertY = this.y;
-        this.move = false;
-        this.speed = 0.1;
-        this.velX = 0;
-        this.velY = 0;
-        this.rotateSpeed = 0.001;
-        this.radius = 15;
-        this.angle = 0;
-    }
-    draw(){
+function Player(){
+    this.x = canvasW / 2;
+    this.y = canvasH / 2;
+    this.vertX = this.x;
+    this.vertY = this.y;
+    this.velX = 0;
+    this.velY = 0;
+    this.radius = 15;
+    this.isMoving = false;
+    this.speed = 0.1;
+    this.rotateSpeed = 0.001;
+    this.angle = 0;
+    this.visible = true;
+
+    this.draw = function(){
         ctx.beginPath();
         let vertAngle = ((Math.PI * 2) / 3);
         let radians = this.angle * 180 / Math.PI;
@@ -71,12 +70,12 @@ class Player{
         ctx.closePath();
         ctx.fill();
     }
-    update(){
+    this.move = function(){
         this.x -= this.velX;
         this.y -= this.velY;
         
         let radians = this.angle * 180 / Math.PI;
-        if (this.move){
+        if (this.isMoving){
             this.velX += Math.cos(radians) * this.speed;
             this.velY += Math.sin(radians) * this.speed;
         }        
@@ -95,23 +94,21 @@ class Player{
         this.velX *= 0.99;
         this.velY *= 0.99;
     }
-    rotate(dir){
+    this.rotate = function(dir){
         this.angle += this.rotateSpeed * dir;
     }
 }
 
-class Asteroid{
-    constructor(x,y,radius,size,collisionRadius){
-        this.x = x || Math.random() * canvasW;
-        this.y = y || Math.random() * canvasH;
-        this.speed = 5;
-        this.radius = radius || 50;
-        this.angle = Math.random();
-        this.strokeColor = 'white';
-        this.collisionRadius = collisionRadius || 47;
-        this.size = size || 1;
-    }
-    draw(){
+function Obstacle(x,y,radius,size,collisionRadius){
+    this.x = x || Math.random() * canvasW;
+    this.y = y || Math.random() * canvasH;
+    this.radius = radius || 50;
+    this.size = size || 1;
+    this.collisionRadius = collisionRadius || 47;
+    this.speed = 5;
+    this.angle = Math.random();
+
+    this.draw = function(){
         ctx.fillStyle = "grey";
         ctx.beginPath();
         let vertAngle = 1;
@@ -122,7 +119,7 @@ class Asteroid{
         ctx.closePath();
         ctx.fill();
     }
-    update(){
+    this.move = function(){
         let radians = this.angle * 180 / Math.PI;
         this.x += Math.cos(radians) * this.speed;
         this.y += Math.sin(radians) * this.speed;
@@ -140,21 +137,20 @@ class Asteroid{
         }
     }
 }
- 
-class Bullet{
-    constructor(angle){
-        this.x = player.vertX;
-        this.y = player.vertY;
-        this.angle = angle;
-        this.height = 5;
-        this.width = 5;
-        this.speed = 5;
-    }
-    draw(){
+
+function Bullet(angle){
+    this.x = player.vertX;
+    this.y = player.vertY;
+    this.height = 5;
+    this.width = 5;
+    this.speed = 5;
+    this.angle = angle;
+
+    this.draw = function(){
         ctx.fillStyle = '#00ff00';
         ctx.fillRect(this.x,this.y,this.width,this.height);
     }
-    update(){
+    this.move = function(){
         let radians = this.angle * 180 / Math.PI;
         this.x -= Math.cos(radians) * this.speed;
         this.y -= Math.sin(radians) * this.speed;
@@ -162,11 +158,7 @@ class Bullet{
 }
  
 function render(){
-
-    player.move = (keys[38]);
-    if (keys[13]){
-       window.location.reload();
-    }
+    player.isMoving = (keys[38]);
     if (keys[39]){
         player.rotate(1);
     }
@@ -177,22 +169,22 @@ function render(){
     ctx.clearRect(0, 0, canvasW, canvasH);
     ctx.fillStyle = 'white';
     ctx.font = '30px Lucida Console';
-    ctx.fillText("SCORE : " + score.toString(), 1200, 50);
+    ctx.fillText("SCORE : " + score.toString(), 1250, 50);
     ctx.fillStyle = 'white';
     ctx.font = '30px Lucida Console';
-    ctx.fillText("LIVES : " + lives.toString(), 1200, 100);
+    ctx.fillText("LIVES : " + lives.toString(), 1250, 100);
 
     if(lives <= 0){
-        document.body.removeEventListener("keydown", keyDown);
-        document.body.removeEventListener("keyup", keyUp);
+        window.removeEventListener("keydown", keyDown);
+        window.removeEventListener("keyup", keyUp);
         player.visible = false;
         ctx.fillStyle = 'white';
         ctx.font = '70px Lucida Console';
         ctx.fillText("GAME OVER", canvasW / 2 - 150, canvasH / 2);
     }
-    if (asteroids.length != 0){
-        for(let i = 0; i < asteroids.length; i++){
-            if(collision(player.x, player.y, player.radius, asteroids[i].x, asteroids[i].y, asteroids[i].collisionRadius)){
+    if (obstacles.length != 0){
+        for(let i = 0; i < obstacles.length; i++){
+            if(collision(player.x, player.y, player.radius, obstacles[i].x, obstacles[i].y, obstacles[i].collisionRadius)){
                 player.x = canvasW / 2;
                 player.y = canvasH / 2;
                 player.velX = 0;
@@ -200,29 +192,29 @@ function render(){
                 lives -= 1;
             }
         }
-    }else if (asteroids.length == 0){
+    }else if (obstacles.length == 0){
         player.x = canvasW / 2;
         player.y = canvasH / 2;
         player.velX = 0;
         player.velY = 0;
         for(let i = 0; i < 8; i++){
-            let asteroid = new Asteroid();
-            asteroid.speed += 1;
-            asteroids.push(asteroid);
+            let obstacle = new Obstacle();
+            obstacle.speed += 1;
+            obstacles.push(obstacle);
         }
     }
-    if (asteroids.length != 0 && bullets.length != 0){
-        for(let i = 0; i < asteroids.length; i++){
+    if (obstacles.length != 0 && bullets.length != 0){
+        for(let i = 0; i < obstacles.length; i++){
             for(let j = 0; j < bullets.length; j++){
-                if(collision(bullets[j].x, bullets[j].y, bullets[j].width, asteroids[i].x, asteroids[i].y, asteroids[i].collisionRadius)){
-                    if(asteroids[i].size == 1){
-                        asteroids.push(new Asteroid(asteroids[i].x - 5, asteroids[i].y - 5, 30, 2, 27));
-                        asteroids.push(new Asteroid(asteroids[i].x + 5, asteroids[i].y + 5, 30, 2, 27));
-                    } else if(asteroids[i].size == 2){
-                        asteroids.push(new Asteroid(asteroids[i].x - 5, asteroids[i].y - 5, 20, 3, 17));
-                        asteroids.push(new Asteroid(asteroids[i].x + 5, asteroids[i].y + 5, 20, 3, 17));
+                if(collision(bullets[j].x, bullets[j].y, bullets[j].width, obstacles[i].x, obstacles[i].y, obstacles[i].collisionRadius)){
+                    if(obstacles[i].size == 1){
+                        obstacles.push(new Obstacle(obstacles[i].x - 5, obstacles[i].y - 5, 30, 2, 27));
+                        obstacles.push(new Obstacle(obstacles[i].x + 5, obstacles[i].y + 5, 30, 2, 27));
+                    } else if(obstacles[i].size == 2){
+                        obstacles.push(new Obstacle(obstacles[i].x - 5, obstacles[i].y - 5, 20, 3, 17));
+                        obstacles.push(new Obstacle(obstacles[i].x + 5, obstacles[i].y + 5, 20, 3, 17));
                     }
-                    asteroids.splice(i,1);
+                    obstacles.splice(i,1);
                     bullets.splice(j,1);
                     score += 10;
                     break;
@@ -232,18 +224,18 @@ function render(){
     }
     if(player.visible){
         player.draw();
-        player.update();
+        player.move();
     }
-    if (asteroids.length != 0){
-        for(let i = 0; i < asteroids.length; i++){
-            asteroids[i].draw();
-            asteroids[i].update();
+    if (obstacles.length != 0){
+        for(let i = 0; i < obstacles.length; i++){
+            obstacles[i].draw();
+            obstacles[i].move();
         }
     }
     if (bullets.length != 0){
         for(let i = 0; i < bullets.length; i++){
             bullets[i].draw();
-            bullets[i].update();
+            bullets[i].move();
         }
     }
     requestAnimationFrame(render);
